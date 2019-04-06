1d25ebbc3f02b0c084e214279c3898d284f5dc8f
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2019-04-06 18:50:56 +0200

    Add start and stop function responsible for clock animation

--- dev/clockJs/clock.js
+++ dev/clockJs/clock.js
@@ -2,6 +2,7 @@
 class Clock {
 
     constructor(clockElement) {
+
         this.clock = clockElement;
         this.hourPointer = this.clock.childNodes[19];
         this.minutePointer = this.clock.childNodes[21];
@@ -12,6 +13,8 @@ class Clock {
         this.actualHour = this.actualTime.getHours();
         this.actualMinutes = this.actualTime.getMinutes();
         this.actualSeconds = this.actualTime.getSeconds();
+        this.animation = null;
+
     }
 
     updateClockWidth() {
@@ -38,86 +41,141 @@ class Clock {
         this.actualSeconds = this.actualTime.getSeconds();
     }
 
+    /**
+     * Updating hour pointer posiotion
+     */
     updateHourPointerPosition() {
 
         let hourTransformationDegrees = 0;
 
+        // Calculate rotate degres
         if (this.actualHour > 12) {
+            // Degress for hour
             let deg = (this.actualHour - 12) * 30;
+            // Degress for minutes
             let mdeg = 30 / (360 / (this.actualMinutes * 6));
+            // Adding both
             hourTransformationDegrees = deg + mdeg;
         } else {
+            // Degress for hour
             let deg = this.actualHour * 30;
+            // Degress for minutes
             let mdeg = 30 / (360 / (this.actualMinutes * 6));
+            // Adding both
             hourTransformationDegrees = deg + mdeg;
         }
 
+        // Setting new rotation angle
         this.hourPointer.style.transform = `rotate(${hourTransformationDegrees}deg)`;
 
+        // Caclculation of new coordinates for pointer in a circle
         let cx = this.clockWidth / 2;
         let cy = this.clockHeight / 2;
 
         let r = this.clockWidth / 2 - (this.clockWidth * 0.07);
 
-        let x = r * Math.cos((hourTransformationDegrees - 90) * Math.PI / 180) + this.clockWidth/2;
-        let y = r * Math.sin((hourTransformationDegrees - 90) * Math.PI / 180) + this.clockWidth/2;
+        let x = r * Math.cos((hourTransformationDegrees - 90) * Math.PI / 180) + this.clockWidth / 2;
+        let y = r * Math.sin((hourTransformationDegrees - 90) * Math.PI / 180) + this.clockWidth / 2;
 
-
+        // Seting new pointer position
         this.hourPointer.style.left = `${x + this.hourPointer.clientWidth}px`;
         this.hourPointer.style.top = `${y}px`;
 
     }
 
+    /**
+    * Updating minute pointer posiotion
+    */
     updateMinutePointerPosition() {
 
         let minuteTransformationDegrees = 0;
 
+        // Calculate rotate degres
         let deg = this.actualMinutes * 6;
 
+        // Setting new rotation angle
         this.minutePointer.style.transform = `rotate(${deg}deg)`;
 
-
+        // Caclculation of new coordinates for pointer in a circle
         let cx = this.clockWidth / 2;
         let cy = this.clockHeight / 2;
 
         let r = this.clockWidth / 2 - (this.clockWidth * 0.02);
 
-        let x = r * Math.cos((deg - 90) * Math.PI / 180) + this.clockWidth/2;
-        let y = r * Math.sin((deg - 90) * Math.PI / 180) + this.clockWidth/2;
+        let x = r * Math.cos((deg - 90) * Math.PI / 180) + this.clockWidth / 2;
+        let y = r * Math.sin((deg - 90) * Math.PI / 180) + this.clockWidth / 2;
 
-
-        this.minutePointer.style.left = `${x }px`;
+        // Seting new pointer position
+        this.minutePointer.style.left = `${x}px`;
         this.minutePointer.style.top = `${y}px`;
 
     }
 
+    /**
+    * Updating seconds pointer posiotion
+    */
     updateSecondPointerPosition() {
 
         let secondTransformationDegrees = 0;
 
+        // Calculate rotate degres
         let deg = this.actualSeconds * 6;
 
+        // Seting new rotation angle
         this.secondsPointer.style.transform = `rotate(${deg}deg)`;
 
-
+        // Caclculation of new coordinates for pointer in a circle
         let cx = this.clockWidth / 2;
         let cy = this.clockHeight / 2;
 
         let r = this.clockWidth / 2 - (this.clockWidth * 0.17);
 
-        let x = r * Math.cos((deg - 90) * Math.PI / 180) + this.clockWidth/2;
-        let y = r * Math.sin((deg - 90) * Math.PI / 180) + this.clockWidth/2;
+        let x = r * Math.cos((deg - 90) * Math.PI / 180) + this.clockWidth / 2;
+        let y = r * Math.sin((deg - 90) * Math.PI / 180) + this.clockWidth / 2;
 
-
+        // Seting new pointer position
         this.secondsPointer.style.left = `${x + this.secondsPointer.clientWidth}px`;
         this.secondsPointer.style.top = `${y}px`;
 
     }
 
+    /**
+     * Updating time values then updates pointers positions
+     */
+    animationFrame() {
 
-    showData() {
+        // Updating times
+        this.updateTime();
+        this.updateHour();
+        this.updateMinutes();
+        this.updateSeconds();
+
+        // Updating pointers position
+        this.updateHourPointerPosition();
+        this.updateMinutePointerPosition();
+        this.updateSecondPointerPosition();
 
     }
+
+    /**
+     * Starts clock animation
+     */
+    startAnimation() {
+
+        this.animation = setTimeout(() => {
+            window.requestAnimationFrame(this.animationFrame.bind(this));
+            this.startAnimation();
+        }, 1);
+
+    }
+
+    /**
+     * Stops clock animation
+     */
+    stopAnimation() {
+        clearTimeout(this.animation);
+    }
+
 }
 
 
--- dev/mainJs.js
+++ dev/mainJs.js
@@ -5,41 +5,4 @@ const Clock = require(`./clockJs/clock`);
 
 const clock = new Clock.Clock(document.querySelector(`.clock`));
 
-clock.showData()
-// clock.updateHourPointerPosition();
-// clock.updateMinutePointerPosition();
-// clock.updateSecondPointerPosition();
-
-// ------------------------------
-// ------------------------------
-// ------------------------------
-
-// console.log(document.querySelector(`.clock`).childNodes[19]);
-
-
-// Proba animacji
-
-const proba = () => {
-
-    clock.updateTime();
-    clock.updateHour();
-    clock.updateMinutes();
-    clock.updateSeconds();
-
-    clock.updateHourPointerPosition();
-    clock.updateMinutePointerPosition();
-    clock.updateSecondPointerPosition();
-
-
-}
-
-const proba2 = () => {
-
-    window.requestAnimationFrame(proba);
-    setTimeout(() => {
-        proba2();
-    }, 1);
-
-}
-
-proba2();
+clock.startAnimation();
